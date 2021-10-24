7fb36e426dcb5ae4c0ab6af00d5280619bc9f04a
Travis-Sherman <trav62801@gmail.com>
2021-10-24 13:57:04 -0400

    hw#4 v1.0

--- website1/src/Drip.js
+++ website1/src/Drip.js
@@ -1,17 +1,14 @@
 import './App.css'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
 import Home from './App.js'
-import Albums from './Albums';
+import Drip1 from './images/gunnaDrip1.jpg';
+import Drip2 from './images/gunnaDrip2.jpg';
+import Drip3 from './images/gunnaDrip3.jpg';
 
 
 export default function Drip(){
 
     return(
+
         <div>
             <header>
                 <h1>Drip</h1>
@@ -22,9 +19,23 @@ export default function Drip(){
                     <a className="currenttab" href="/Drip">Drip</a>
                 </nav>
             </header>
-            <body>
-                <h2>Gunna's outifts influence a lot of people inclding me.</h2>
-                <p>Some of my favorite pieces of his are the Amiri Skelton bone sneakers, which I purchased myself, and Dior Shirst and Pants</p>
+            <body id="body">
+                <div id = "image">
+                    Hover over an image below to display here.
+                </div>
+	
+                <img class = "preview" src={Drip1} alt="Wool Coat"
+                    onMouseEnter={() => upDatePic(Drip1, 'Wool Coat')}
+                    onMouseOut={() => unDo()}/>
+                
+                <img class = "preview" src={Drip2} alt="B/W Nikes" 
+                    onMouseEnter={() => upDatePic(Drip2, 'B/W Nikes')}
+                    onMouseOut={() => unDo()}/>
+                
+                <img class = "preview" src={Drip3} alt="B/W LV Shoes" 
+                    onMouseEnter={() => upDatePic(Drip3, 'B/W LV Shoes')} 
+                    onMouseOut={() => unDo()}/>
+
             </body>
 
             <footer>
@@ -32,4 +43,15 @@ export default function Drip(){
             </footer>
         </div>
     );
+}
+
+function upDatePic(previewPic, previewAlt) {
+    document.getElementById('image').innerHTML = previewAlt ;
+    document.getElementById('image').style.backgroundImage = "url(" + previewPic + ")";
+}
+
+function unDo() {
+
+    document.getElementById('image').style.backgroundImage = "url('')"
+    document.getElementById('image').innerHTML = 'Hover over an image below to display here.';
 }
